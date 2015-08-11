4da6d33ab9da45cecf071d35025826b833e6f342
NinJa1 <NinJa1@NinJa1deMac-mini.local>
2015-08-11 09:29:52 +0800

    Initial Commit

---
+++ project_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  project_2
+//
+//  Created by NinJa1 on 15/8/11.
+//  Copyright (c) 2015年 NinJa1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ project_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  project_2
+//
+//  Created by NinJa1 on 15/8/11.
+//  Copyright (c) 2015年 NinJa1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
